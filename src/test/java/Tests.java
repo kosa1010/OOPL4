e83ea0f48c5a5bcc8d40de17edec4fb06e2e361c
Dawid Kosior <kosiordawid@gmail.com>
2024-03-27 12:42:39 +0100

    Update Tests.java
    tests updates

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -8,13 +8,16 @@ import pl.edu.ur.oopl4.zad3.figures.flat.Square;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.util.Scanner;
+
 import pl.edu.ur.oopl4.zad3.figures.flat.Rectangle;
 import pl.edu.ur.oopl4.zad3.figures.spatial.Cone;
 import pl.edu.ur.oopl4.zad3.figures.spatial.Cube;
 import pl.edu.ur.oopl4.zad3.figures.spatial.Cuboid;
 import pl.edu.ur.oopl4.zad3.figures.spatial.Sphere;
+import pl.edu.ur.oopl4.zad4.StudentGroup;
 
 /**
+ *
  */
 public class Tests {
 
@@ -125,25 +128,67 @@ public class Tests {
     @Test(timeout = 10000)
     public void testCubeSize() {
         Cube cube = new Cube(2);
-        Assert.assertEquals(Math.round(Math.pow(2, 3)), Math.round(cube.size()));
+        Assert.assertEquals(Math.round(Math.pow(2, 3)), Math.round(cube.volume()));
     }
 
     @Test(timeout = 10000)
     public void testConeSize() {
         Cone cone = new Cone(2, 2);
-        Assert.assertEquals(Math.round((1.0 / 3.0) * Math.PI * Math.pow(2, 2) * 2), Math.round(cone.size()));
+        Assert.assertEquals(Math.round((1.0 / 3.0) * Math.PI * Math.pow(2, 2) * 2), Math.round(cone.volume()));
     }
 
     @Test(timeout = 10000)
     public void testCuboidSize() {
         Cuboid cuboid = new Cuboid(2, 3, 4);
-        Assert.assertEquals(Math.round(2 * 3 * 4), Math.round(cuboid.size()));
+        Assert.assertEquals(Math.round(2 * 3 * 4), Math.round(cuboid.volume()));
     }
 
     @Test(timeout = 10000)
     public void testSphereSize() {
         Sphere sphere = new Sphere(2);
-        Assert.assertEquals(Math.round((4.0 / 3) * Math.PI * Math.pow(2, 3)), Math.round(sphere.size()));
+        Assert.assertEquals(Math.round((4.0 / 3) * Math.PI * Math.pow(2, 3)), Math.round(sphere.volume()));
     }
 
+
+    @Test(timeout = 10000)
+    public void testEx4() throws NullPointerException {
+
+        Student s1 = new Student("Jan", "Nowak", 8234, "ISWD", 1);
+        Student s20 = new Student("Grażyna", "Zeszczecina", 4434, "ISWD", 2);
+        Student s31 = new Student("Tomasz", "Kowalski", 3334, "ISWD", 3);
+        Student s99 = new Student("Janusz", "Gągorzelewski", 1114, "ISWD", 3);
+
+        StudentGroup sg = new StudentGroup();
+        sg.init();
+
+        sg.setStudentDataAtIndex(1, s1.getName(), s1.getSurname(), s1.getIndexNumber(), s1.getSpeciality(), s1.getYearOfStudy());
+        sg.setStudentDataAtIndex(20, s20.getName(), s20.getSurname(), s20.getIndexNumber(), s20.getSpeciality(), s20.getYearOfStudy());
+        sg.setStudentDataAtIndex(31, s31.getName(), s31.getSurname(), s31.getIndexNumber(), s31.getSpeciality(), s31.getYearOfStudy());
+        sg.setStudentDataAtIndex(99, s99.getName(), s99.getSurname(), s99.getIndexNumber(), s99.getSpeciality(), s99.getYearOfStudy());
+
+        Assert.assertEquals("Jan", sg.group[1].getName());
+        Assert.assertEquals("Nowak", sg.group[1].getSurname());
+        Assert.assertEquals(8234, sg.group[1].getIndexNumber());
+        Assert.assertEquals("ISWD", sg.group[1].getSpeciality());
+        Assert.assertEquals(1, sg.group[1].getYearOfStudy());
+
+        String s2 = sg.getDataOfAllStudents().split("\n")[20];
+        Assert.assertEquals(true, s2.contains("Grażyna"));
+        Assert.assertEquals(true, s2.contains("Zeszczecina"));
+        Assert.assertEquals(true, s2.contains("4434"));
+        Assert.assertEquals(true, s2.contains("ISWD"));
+        Assert.assertEquals(true, s2.contains("2"));
+
+        sg.setDefaultDataAtIndex(20);
+        Assert.assertEquals("", sg.group[20].getName());
+        Assert.assertEquals("", sg.group[20].getSurname());
+        Assert.assertEquals(0, sg.group[20].getIndexNumber());
+        Assert.assertEquals("", sg.group[20].getSpeciality());
+        Assert.assertEquals(0, sg.group[20].getYearOfStudy());
+
+        String s9 = sg.getStudentDataAtIndex(99).toString();
+        Assert.assertEquals(true, s9.contains("Janusz"));
+
+        Assert.assertEquals(true, sg.getDataOfAllStudents().contains(sg.getStudentsFromRange(30,32)));
+    }
 }
